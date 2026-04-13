3a7d495733b71bf9f51c604e1064fe15ebb44348
doly-dev-001 <doly-dev-1@qq.com>
2026-04-13 21:26:15 +0800

    Remove accidental backup directory libs/FaceReco/FaceReco_backup_*

--- libs/FaceReco/FaceReco_backup_1776086623/src/DatasetHandler/dataset_handler.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "abstract_dataset_handler.hpp"
-#include "descriptor_dataset_handler.hpp"
-#include "image_dataset_handler.hpp"
--- libs/FaceReco/FaceReco_backup_1776086623/src/defs.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-#define PI 3.14159265
-#define PROJECT_PATH "/home/pi/dolydev/libs/FaceReco"
