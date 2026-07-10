97bf4b1924a162920a09cc8986182818c5265f09
doly-dev-001 <doly-dev-1@qq.com>
2026-07-10 17:15:42 +0800

    sudo apt install libyaml-cpp-dev

--- scripts/update_and_redeploy.sh
+++ scripts/update_and_redeploy.sh
@@ -157,7 +157,7 @@ main() {
     sync_repository
 
     print_status "更新python虚拟环境..."
-    sudo apt-get install -y python3-venv libspdlog-dev
+    sudo apt-get install -y python3-venv libspdlog-dev libyaml-cpp-dev
     local venv_path="/home/pi/dolydev/.venv"
     local requirements_file="/home/pi/dolydev/libs/requirements.txt"
     
